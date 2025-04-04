00a972ce4b3520d6bd839295944e3a52687741d6
LitoSaez <saezlito422@gmail.com>
2025-04-04 19:29:57 +0800

    Update images.ts

--- constants/images.ts
+++ constants/images.ts
@@ -1,4 +1,4 @@
-import onboarding from "@/assets/images/onboarding.png";
+import wallpaper from "@/assets/images/wallpaper.jpg";
 import avatar from "@/assets/images/avatar.png";
 import newYork from "@/assets/images/new-york.png";
 import japan from "@/assets/images/japan.png";
@@ -9,7 +9,7 @@ import map from "@/assets/images/map.png";
 import noResult from "@/assets/images/no-result.png";
 
 export default {
-  onboarding,
+  wallpaper,
   avatar,
   newYork,
   japan,
